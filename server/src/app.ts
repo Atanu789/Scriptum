abf916795fe0c338a47509d0a1f1f64abccb699b
Atanu789 <atanurandhawa@gmail.com>
2026-03-21 19:39:19 +0530

    feat: add dotenv configuration for environment variable management

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,6 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
+import dotenv from "dotenv";
+dotenv.config();
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
